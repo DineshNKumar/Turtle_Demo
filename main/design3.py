323ae0572459b54b540637789b8029351edb9a72
Dinesh Kumar <dnkumar0510@gmail.com>
2020-08-29 04:15:34 -0700

    Commit

--- main/design3.py
+++ main/design3.py
@@ -1,20 +1,17 @@
 import turtle
+import math
 
 
 def square(des, size):
-    if size <= 10:
-        return
-    else:
-        for i in range(1,5):
-            des.forward(20)
-            des.left(90)
+    for j in range(50):
+        des.forward(100)
+        des.left(size)
+        size += 1
 
 
 if __name__ == '__main__':
-
     des = turtle.Turtle()
 
-    des.speed(20)
     square(des, 100)
 
     turtle.done()
